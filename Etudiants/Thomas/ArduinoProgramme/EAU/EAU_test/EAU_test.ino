7d9bdf6ce6255dbd31bf759d7ce51b9a2dc51476
ThomasCadeau <56728406+ThomasCadeau@users.noreply.github.com>
2020-03-25 15:46:19 +0100

    arduino test

--- Etudiants/Thomas/ArduinoProgramme/EAU/EAU_test/EAU_test.ino
+++ Etudiants/Thomas/ArduinoProgramme/EAU/EAU_test/EAU_test.ino
@@ -9,9 +9,9 @@ long debounce = 2000; // Temps de déparasitage (debounce time), a augmenter si 
 
 void setup() {
   Serial.begin (9600);// Démarrage de la liaison série
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT); //LED H2O
+  pinMode(3, OUTPUT);//RELAIS H2O FRIGO
+  pinMode(4, OUTPUT);//FONTAINE
 
   digitalWrite(4, HIGH);
 
@@ -25,55 +25,7 @@ void loop() {
   
   readingH2O = analogRead (CapteurHum);// Lecture de la valeur du capteur
   
-  
-  if (etateau == 0)
-  {
-    H2OInitial = readingH2O;
-    etateau = 1;
-  }
-  
- 
-  if (etateau == 1)
-  {
-    if (readingH2O >= (H2OInitial + 180)) {
-      var1 = 1;}
-      else { var1 = 0;}
-
-      delay(3000);
-      
- if (readingH2O >= (H2OInitial + 180)) {
-  var2 = 1;}
- else { var2 = 0;}
-
-      
-
-    if (var1 == var2 && var2 == 1)
-    {
-      etateau = 2;
-      digitalWrite(2, HIGH); //LED H2O
-      digitalWrite(4, LOW); //FONTAINE
-      digitalWrite(3, LOW);  //RELAIS H2O FRIGO
-      delay(1000);
-      digitalWrite(3, HIGH);
-      delay(1000);
-      digitalWrite(3, LOW);  //RELAIS H2O FRIGO
-      delay(1000);
-      digitalWrite(3, HIGH);
-      delay(1000);
-      digitalWrite(3, LOW);
-      delay(1000);
-      digitalWrite(3, HIGH);
-      delay(5000);
-      digitalWrite(3, LOW);
-      delay(20000);
-    }
-  }
-  digitalWrite(3, HIGH);
-
-
-Serial.println(readingH2O);
-
-}
-
+  Serial.println(CapteurHum);
+  delay(300);
 
 }
