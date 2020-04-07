b514f520fc51315e511ab43cb7e5bef2c6a2d44b
ConstantinMinosSN2 <56729273+ConstantinMinosSN2@users.noreply.github.com>
2020-04-07 14:42:54 +0200

    +

--- Etudiants/Constantin/Prog_Arduino/Arduino_1/Etape5_EtudeMecanisme4/Mecanisme4/Mecanisme4.ino
+++ Etudiants/Constantin/Prog_Arduino/Arduino_1/Etape5_EtudeMecanisme4/Mecanisme4/Mecanisme4.ino
@@ -2,28 +2,22 @@
 
 #define SLAVE_ADDRESS 0x15 // initialisation de l’Arduino avec l’adresse 0x15
 
-bool mechanism_status = true;
-
 bool S_Dragon = false;
 bool S_Fumee = false;
 bool S_Led = false;
 bool S_Feu = true;
 bool actuator[] = {S_Dragon, S_Fumee, S_Led, S_Feu};
 
+bool C_Interupteur = false;
+bool sensor[] = {C_Interupteur};
 
 
-/*void execute_order(String order){
-  if(order[3] == 'T'){
-    mechanism_status = true;
-  }else if(order[3] == 'F'){
-    mechanism_status = false;
-  }
-
-  for(int i=6; i<sizeof(order)+6; i++) {
+void execute_order(String order){
+  for(int i=1; i<sizeof(order)+1; i++) {
     if(order[i] == 'T'){
-      actuator[i-6] = true;
+      actuator[i-1] = true;
     }else if(order[i] == 'F'){
-      actuator[i-6] = false;
+      actuator[i-1] = false;
     }
   }
 }
@@ -44,16 +38,9 @@ void receive_order(int byteCount) {
 }
 
 void send_status() {
-  String ms_I2Cmessage = "MS";
-  String as_I2Cmessage = "AS";
+  String as_I2Cmessage = "as";
+  String sd_I2Cmessage = "sd";
   String I2Cmessage;
-  
-  if(mechanism_status == true){
-    ms_I2Cmessage += "T";
-  }
-  else{
-    ms_I2Cmessage += "F";
-  }
 
   for(int i=0; i<sizeof(actuator); i++){
     if (actuator[i] == true){
@@ -62,21 +49,33 @@ void send_status() {
       as_I2Cmessage += "F";
     }
   }
+  if (as_I2Cmessage.length() < 6){
+    for(int i=as_I2Cmessage.length()-1; i<5; i++){
+      as_I2Cmessage += "X";
+    }
+  }
   
-  I2Cmessage = ms_I2Cmessage + as_I2Cmessage;//MSTASFFFT
+  for(int i=0; i<sizeof(sensor); i++){
+    if (sensor[i] == true){
+      sd_I2Cmessage += "T";
+    }else{
+      sd_I2Cmessage += "F";
+    }
+  }
+
+  I2Cmessage = as_I2Cmessage + sd_I2Cmessage;//asFFXXsdF
   
   Wire.write(I2Cmessage.c_str());
   Serial.print("Message send to Raspberry : ");
   Serial.println(I2Cmessage);
 }
 
-
 void setupI2C() {
     Serial.begin(9600);
     Wire.begin(SLAVE_ADDRESS);
     Wire.onReceive(receive_order);
     Wire.onRequest(send_status);
-}*/
+}
 
 void setup() {
     //setupI2C();
--- Etudiants/Constantin/Prog_Arduino/Arduino_2/Etape5_EtudeMecanisme8/Mecanisme8/Mecanisme8.ino
+++ Etudiants/Constantin/Prog_Arduino/Arduino_2/Etape5_EtudeMecanisme8/Mecanisme8/Mecanisme8.ino
@@ -2,24 +2,21 @@
 
 #define SLAVE_ADDRESS 0x19 // initialisation de l’Arduino avec l’adresse 0x19
 
-bool mechanism_status = false;
 
 bool S_Tableau = false;
 bool S_Led = false;
 bool actuator[] = {S_Tableau, S_Led};
 
-void execute_order(String order){
-  if(order[3] == 'T'){
-    mechanism_status = true;
-  }else if(order[3] == 'F'){
-    mechanism_status = false;
-  }
+int C_Poids = 42;
+int sensor[] = {C_Poids};
 
-  for(int i=6; i<sizeof(order)+6; i++) {
+
+void execute_order(String order){
+  for(int i=1; i<sizeof(order)+1; i++) {
     if(order[i] == 'T'){
-      actuator[i-6] = true;
+      actuator[i-1] = true;
     }else if(order[i] == 'F'){
-      actuator[i-6] = false;
+      actuator[i-1] = false;
     }
   }
 }
@@ -40,16 +37,9 @@ void receive_order(int byteCount) {
 }
 
 void send_status() {
-  String ms_I2Cmessage = "MS";
-  String as_I2Cmessage = "AS";
+  String as_I2Cmessage = "as";
+  String sd_I2Cmessage = "sd";
   String I2Cmessage;
-  
-  if(mechanism_status == true){
-    ms_I2Cmessage += "T";
-  }
-  else{
-    ms_I2Cmessage += "F";
-  }
 
   for(int i=0; i<sizeof(actuator); i++){
     if (actuator[i] == true){
@@ -58,20 +48,34 @@ void send_status() {
       as_I2Cmessage += "F";
     }
   }
-  
-  I2Cmessage = ms_I2Cmessage + as_I2Cmessage;//MSTASFFFT
+  if (as_I2Cmessage.length() < 6){
+    for(int i=as_I2Cmessage.length()-1; i<5; i++){
+      as_I2Cmessage += "X";
+    }
+  }
+
+  for(int i=0; i<sizeof(sensor)/2; i++){
+    sd_I2Cmessage += sensor[i];
+    sd_I2Cmessage += "X";
+  }
+
+  I2Cmessage = as_I2Cmessage + sd_I2Cmessage;//asFFXXsd0
   
   Wire.write(I2Cmessage.c_str());
   Serial.print("Message send to Raspberry : ");
   Serial.println(I2Cmessage);
 }
 
-
-void setup() {
+void setupI2C() {
     Serial.begin(9600);
     Wire.begin(SLAVE_ADDRESS);
     Wire.onReceive(receive_order);
     Wire.onRequest(send_status);
+}
+
+
+void setup() {
+    //setupI2C();
 }
 
 void loop() {
