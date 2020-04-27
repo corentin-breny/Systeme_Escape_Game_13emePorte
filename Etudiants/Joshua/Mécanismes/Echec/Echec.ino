6e59653414da0d55831862dbb2b3b97d57a7c48b
JoshuaPinneauSN2 <56728286+JoshuaPinneauSN2@users.noreply.github.com>
2020-04-27 16:51:28 +0200

    Travail sur les mécanismes Echec et LionB

--- Etudiants/Joshua/Mécanismes/Echec/Echec.ino
+++ Etudiants/Joshua/Mécanismes/Echec/Echec.ino
@@ -7,30 +7,87 @@
 class Echec{
 
   private :
-    bool C_EffetHall1;
-    bool C_EffetHall2;
+    const int C_EffetHall1;
+    const int C_EffetHall2;
     bool S_LedR;
     bool mecanism_status;
+    int pin1;
+    int pin2;
 
   private :
     bool actuator[3] = {S_LedR};
     bool sensor[1] = {C_EffetHall1};
-    bool sensor[2] = {C_EffetHall2};    
+    bool sensor[2] = {C_EffetHall2};
+    int ECHECstate = HIGH; // Etat actuel de la broche de sortie (la ventouse HIGH maintiens de la ventouse fermée)
+    int readingECHEC;         // Contient la valeur lue sur la broche d'entrée
+    int previousECHEC = LOW;  // Contient la précédente valeur lue sur la broche d'entrée
+    long timeECHEC = 0;      // La dernière fois que la broche de sortie à changé d'état
+    long time = 0;      // La dernière fois que la broche de sortie à changé d'état
+    long debounce = 2000; // Temps de déparasitage (debounce time), a augmenter si la sortie clignote.     
 
   public :
     Echec();
     void setupMecanism();
     void execute();
     void receive_order();
-    void send_status();    
+    void send_status();
+   
 };
 
 //Fichier Echec.cpp
 
 Echec::Echec(){
   
-  C_EffetHall1 = false;
-  C_EffetHall2 = false;
+  C_EffetHall_1 = false;
+  C_EffetHall_2 = false;
   S_LedR = false;
   mecanism_status = false;
+}
+
+
+void Echec::setupMechanism() {
+  
+  pinMode( 8, OUTPUT ); // Relais echec
+  digitalWrite(8, HIGH);
+  
+  pinMode( 7, OUTPUT ); // Led controle echec
+  digitalWrite(7, HIGH);
+
+  pinMode(11, INPUT_PULLUP);
+
+  digitalWrite(hallPin, HIGH);  // Activation résistance pullUP
+  digitalWrite(hallPin2, HIGH);
+ 
+}
+
+void Echec::execute(){
+
+  pin1 = digitalRead(C_EffetHall_1);
+  pin2 = digitalRead(C_EffetHall_2);
+  
+  readingECHEC = pin1;
+
+  if (ECHECstate == HIGH)
+  {
+    if (readingECHEC != previousECHEC) {    
+      
+      timeECHEC = millis();   // Remettre la minuterie/timer de déparasitage à 0
+    }
+    
+    if ((millis() - timeECHEC) > debounce)    // Attendre que l'on ai dépassé le temps de déparasitage
+    {
+      
+      if ((pin1 == LOW) && (pin2 == LOW))     // Si les deux pieces sont à la bonne place
+      {  
+        ECHECstate = LOW; // On valide l'enigme
+      } else LEDstate = HIGH;
+      
+    previousECHEC = readingECHEC;     // Mémoriser la dernière lecture
+    }  
+  }
+
+  digitalWrite(7, not(ECHECstate));   // Écriture du statut sur la led (HIGH = allumé soit l'inverse de la ventouse)
+  digitalWrite(8, ECHECstate);    // Écriture du statut sur la ventouse (LOW = on libere l'electroaimant)
+  delay(500);
+  digitalWrite(8, not(ECHECstate));
 }
--- Etudiants/Joshua/Mécanismes/Lion/Lion.ino
+++ Etudiants/Joshua/Mécanismes/Lion/Lion.ino
@@ -1,70 +1,91 @@
-const int halllionb = 6;
-int pinlionb;
+//Fichier LionB.h
 
-int LIONBstate = HIGH; // Etat actuel de la broche de sortie (la LED)
-int readingLIONB;         // Contient la valeur lue sur la broche d'entrée
-int previousLIONB = LOW;
-long timeLIONB = 0;      // La dernière fois que la broche de sortie à changé d'état
-int LedLIONB = LOW;
-void setup() {
-	
-	pinMode( 8, OUTPUT ); // vers relais echec
-	pinMode( 7, OUTPUT ); // vers led controle echec
-	pinMode(10, OUTPUT); // vers led controle lionb
-	pinMode( 3, OUTPUT); // Vers relais lionb
-	pinMode( halllionb, INPUT); // pin 6
-	pinMode(11, INPUT_PULLUP);
-	pinMode(12, OUTPUT);
-	pinMode(13, OUTPUT);
+#define C_EffetHall1_PIN A0 //Capteur à effet Hall 1
+#define S_LedR_PIN 3 //Led de sortie
 
-	digitalWrite(halllionb, HIGH);
-	digitalWrite(3, HIGH);
-	digitalWrite(8, HIGH);
+class LionB {
+
+  private :
+    const int C_EffetHall1;
+    bool S_LedR;
+    bool mecanism_status;
+    int pin1;
+    int pin2;
+
+  private :
+    bool actuator[3] = {S_LedR};
+    bool sensor[1] = {C_EffetHall1};
+    int LIONBstate = HIGH; // Etat actuel de la broche de sortie (la LED)
+    int readingLIONB;         // Contient la valeur lue sur la broche d'entrée
+    int previousLIONB = LOW;
+    long timeLIONB = 0;      // La dernière fois que la broche de sortie à changé d'état
+    int LedLIONB = LOW;
+
+  public :
+    LionB();
+    void setupMecanism();
+    void execute();
+    void receive_order();
+    void send_status();
+};
+
+//Fichier LionB.cpp
+
+LionB::LionB(){  
+  C_EffetHall_1 = false;
+  S_LedR = false;
+  mecanism_status = false;
 }
 
-void loop() {
-	
-	readingLIONB = pinlionb;
-	
-  // Si le contact à changé, est-ce causé par un changement de
-  // position OU un parasite (bille qui trésaute)...
+void LionB::setupMechanism() {
   
-	if (LIONBstate == HIGH)
-    {
-		if (readingLIONB != previousLIONB) {
-			// Remettre la minuterie/timer de déparasitage à 0
-			timeLIONB = millis();
-        }
-	
-    // attendre que l'on ai dépassé le temps de déparasitage
-	
-    if ((millis() - timeLIONB) > (debounce)) {
-		
-		if (readingLIONB == HIGH) {
-			
-			LIONBstate = LOW;
-			digitalWrite(3, LIONBstate);
-			//reset pour defaire le solenoide
-			timeLIONB = millis();
-			LedLIONB = HIGH;
-			
-        }
+  pinMode(10, OUTPUT); // vers led controle lionb
+  digitalWrite(10, HIGH);
+  
+  pinMode( 3, OUTPUT); // Vers relais lionb
+  digitalWrite(3, HIGH);
+  
+  pinMode(C_EffetHall, INPUT); // pin 6
+  digitalWrite(C_EffetHall, HIGH);
+  
+  pinMode(11, INPUT_PULLUP);
+  
+  digitalWrite(hallPin, HIGH);  // Activation résistance pullUP
+  digitalWrite(hallPin2, HIGH);
+ 
+}
+
+void LionB::execute(){
+  
+  pinlionb = digitalRead(C_EffetHall);
+
+  readingLIONB = pinlionb;
+
+  if (LIONBstate == HIGH)
+  {
+    if (readingLIONB != previousLIONB) 
+    {  
+      timeLIONB = millis();   // Remettre la minuterie/timer de déparasitage à 0
     }
-	
-    // Mémoriser la dernière lecture
-	
-    previousLIONB = readingLIONB;
-	
+    
+    if ((millis() - timeLIONB) > (debounce)) // attendre que l'on ai dépassé le temps de déparasitage
+    {    
+      if (readingLIONB == HIGH) 
+      {
+        LIONBstate = LOW;
+        digitalWrite(3, LIONBstate);    //reset pour defaire le solenoide
+        timeLIONB = millis();
+        LedLIONB = HIGH;
+      }
     }
+
+    previousLIONB = readingLIONB;   // Mémoriser la dernière lecture
+  }
   else if ((millis() - timeLIONB) > debounce)
   {
-	  
     digitalWrite(3, HIGH);
     if (readingLIONB == LOW)
-      LIONBstate = HIGH;
-  
+    LIONBstate = HIGH;
   }
-  
-  digitalWrite(10, LedLIONB);
-  
+  digitalWrite(10, LedLIONB);  
 }
