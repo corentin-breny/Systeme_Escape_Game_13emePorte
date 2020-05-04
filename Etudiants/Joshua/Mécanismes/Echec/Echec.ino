317c37a3b335d601c2ebb544f57cff9a708266d9
JoshuaPinneauSN2 <56728286+JoshuaPinneauSN2@users.noreply.github.com>
2020-05-04 12:23:59 +0200

    Update Echec.ino

--- Etudiants/Joshua/Mécanismes/Echec/Echec.ino
+++ Etudiants/Joshua/Mécanismes/Echec/Echec.ino
@@ -3,35 +3,33 @@
 #define C_EffetHall1_PIN A0 //Capteur à effet Hall 1
 #define C_EffetHall2_PIN 2 //Capteur à effet Hall 2
 #define S_LedR_PIN 3 //Led de sortie
+#define debounce = 2000
+
+int ECHECstate = HIGH; // Etat actuel de la broche de sortie (la ventouse HIGH maintiens de la ventouse fermée)
+int readingECHEC;         // Contient la valeur lue sur la broche d'entrée
+int previousECHEC = LOW;  // Contient la précédente valeur lue sur la broche d'entrée
+long timeECHEC = 0;      // La dernière fois que la broche de sortie à changé d'état
+long time = 0;      // La dernière fois que la broche de sortie à changé d'état 
 
 class Echec{
 
   private :
     const int C_EffetHall1;
     const int C_EffetHall2;
-    bool S_LedR;
+    bool S_Echiquier;
     bool mecanism_status;
-    int pin1;
-    int pin2;
 
-  private :
-    bool actuator[3] = {S_LedR};
-    bool sensor[1] = {C_EffetHall1};
-    bool sensor[2] = {C_EffetHall2};
-    int ECHECstate = HIGH; // Etat actuel de la broche de sortie (la ventouse HIGH maintiens de la ventouse fermée)
-    int readingECHEC;         // Contient la valeur lue sur la broche d'entrée
-    int previousECHEC = LOW;  // Contient la précédente valeur lue sur la broche d'entrée
-    long timeECHEC = 0;      // La dernière fois que la broche de sortie à changé d'état
-    long time = 0;      // La dernière fois que la broche de sortie à changé d'état
-    long debounce = 2000; // Temps de déparasitage (debounce time), a augmenter si la sortie clignote.     
+  public :
+    bool actuator[1] = {S_Echiquier};
+    int sensor[2] = {C_EffetHall2, C_EffetHall2};
+    bool getMechanism_status();
+    void setMechanism_status(bool ms);    
 
   public :
     Echec();
     void setupMecanism();
     void execute();
-    void receive_order();
-    void send_status();
-   
+    
 };
 
 //Fichier Echec.cpp
@@ -40,10 +38,18 @@ Echec::Echec(){
   
   C_EffetHall_1 = false;
   C_EffetHall_2 = false;
-  S_LedR = false;
+  S_Echiquier = false;
   mecanism_status = false;
+  
 }
 
+bool Echec::getMechanism_status(){
+  return mechanism_status;
+}
+
+void Echec::setMechanism_status(bool ms){
+  mechanism_status = ms;
+}
 
 void Echec::setupMechanism() {
   
@@ -62,8 +68,8 @@ void Echec::setupMechanism() {
 
 void Echec::execute(){
 
-  pin1 = digitalRead(C_EffetHall_1);    // On lit les deux capteurs à effet Hall
-  pin2 = digitalRead(C_EffetHall_2);
+  int pin1 = digitalRead(C_EffetHall_1);    // On lit les deux capteurs à effet Hall
+  int pin2 = digitalRead(C_EffetHall_2);
   
   readingECHEC = pin1;
 
@@ -90,4 +96,100 @@ void Echec::execute(){
   digitalWrite(8, ECHECstate);    // Écriture du statut sur la ventouse (LOW = on libere l'electroaimant)
   delay(500);
   digitalWrite(8, not(ECHECstate));
+
+  if ( mechanism_status == false ){          //En cas de reset
+    S_Echiquier = false;                  //On change la valeur de l'attribut
+  }
+}
+
+Echec mechanism = Echec();
+
+void receive_order(int numBytes) {
+  String data_received;
+  
+  while(Wire.available() > 0) {           //Tant que le message n'est pas fini
+    char c = Wire.read();             //On lit le message
+    data_received += String(c);
+  }
+  
+  if(data_received != "2") {
+    
+    String order = data_received;
+    Serial.print("Order received : ");
+    Serial.println(order);//412221
+
+    if(order[1] == '1'){              //Si le 2eme caractère est 1
+      mechanism.setMechanism_status(true);    //On valide le mécanisme
+    }else if(order[1] == '0'){            //Si le 2eme caractère est 0
+      mechanism.setMechanism_status(false);   //On invalide le mécanisme
+    }
+    
+    for(int i=2; i<sizeof(order); i++) {      //Pour chaque actionneur
+      if(order[i] == '1'){            //Si le caractère est 1
+        mechanism.actuator[i-1] = true;     //On valide l'actionneur
+      }else if(order[i] == '0'){          //Si le caractère est 0
+        mechanism.actuator[i-1] = false;    //On invalide l'actionneur
+      }
+    }
+  }
+}
+
+void send_status() {
+  String ms_I2Cmessage = "ms";
+  String as_I2Cmessage = "as";
+  String sd_I2Cmessage = "sd";
+  String I2Cmessage;
+
+  if(mechanism.getMechanism_status() == true){    //Si le mécanisme est validé
+    ms_I2Cmessage += "T";             //On ajoute T au message i2c
+  }else{                        //Si le mécanisme est invalide
+    ms_I2Cmessage += "F";             //On ajoute F au message i2c
+  }
+  
+  for(int i=0; i<sizeof(mechanism.actuator); i++){  //Pour chaque actionneur du mécanisme
+    if (mechanism.actuator[i] == true){       //Si l'actionneur est validé
+      as_I2Cmessage += "T";           //On ajoute T au message i2c
+    }else{                      //Si l'actionneur est invalidé
+      as_I2Cmessage += "F";           //On ajoute F au message i2c
+    }
+  }
+  if(as_I2Cmessage.length() < 6){
+    for(int i=as_I2Cmessage.length()-1; i<5; i++){  //Tant que le message est inférieur à 6 caractere
+      as_I2Cmessage += "X";           //On ajoute X au message i2c
+    }
+  }
+
+  /*//A COMMENTER SI LES CAPTEURS SONT DES VALEURS BOOLEAN
+  for(int i=0; i<sizeof(mechanism.sensor); i++){    //Pour chaque capteur du mécanisme
+    if (mechanism.sensor[i] == true){       //Si le capteur est validé
+      sd_I2Cmessage += "T";           //On ajoute T au message i2c
+    }else{                      //Si le capteur est invalidé
+      sd_I2Cmessage += "F";           //On ajoute F au message i2c
+    }
+  }*/
+
+  //A COMMENTER SI LES CAPTEURS SONT DES VALEURS NUMERIQUE
+  for(int i=0; i<sizeof(sensor)/2; i++){        //Pour chaque capteur du mécanisme
+    sd_I2Cmessage += sensor[i];           //On ajoute la valeur du capteur au message i2c
+    sd_I2Cmessage += "X";             //Et on ajoute aussi X
+  }
+  
+  I2Cmessage = ms_I2Cmessage + as_I2Cmessage + sd_I2Cmessage;//msFasFFFFsdF
+  
+  Wire.write(I2Cmessage.c_str());           //On envoie le message i2c
+  Serial.print("Message send to Raspberry : ");
+  Serial.println(I2Cmessage);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receive_order);
+  Wire.onRequest(send_status);
+  mechanism.setupMechanism();
+}
+
+void loop() {
+  delay(100);                     //On attends 0.1 seconde
+  mechanism.execute();                //On exécute le mécanisme
 }
